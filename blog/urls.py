376ab37417049aeca4ef6de34715b507b7886b3f
mara <maracomkr@gmail.com>
2017-05-10 17:46:11 +0900

    Post form and edit

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), #http://127.0.0.1:8000/post/1/
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
