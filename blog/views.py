4faca4ae1f59458dd91dd673feb911f5cbc95ee5
mara <maracomkr@gmail.com>
2017-05-10 17:08:13 +0900

    static files

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,9 @@ from .models import Post
 from django.contrib.auth.models import User
 
 admin = User.objects.get(username='admin')
+me2 = User.objects.get(username='me')
 
 def post_list(request):
-	posts = Post.objects.filter(author=admin)
-	#posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#posts = Post.objects.filter(author=me2)
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')  #publish 된것만 나온다
 	return render(request, 'blog/post_list.html', {'posts': posts})
